9361c42bb3757739c36a53f304525e52adee48e5
Gabriel Carmelo Villaverde <vgabrielcv@gmail.com>
2023-10-08 22:35:54 -0500

    Billboards, categories, colors and sizes done

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -19,6 +19,26 @@ export const MainNav = ({
             active: pathname === `/${params.storeId}`
         },
         {
+            href: `/${params.storeId}/billboards`,
+            label: 'Billboards',
+            active: pathname === `/${params.storeId}/billboards`
+        },
+        {
+            href: `/${params.storeId}/categories`,
+            label: 'Categories',
+            active: pathname === `/${params.storeId}/categories`
+        },
+        {
+            href: `/${params.storeId}/sizes`,
+            label: 'Sizes',
+            active: pathname === `/${params.storeId}/sizes`
+        },
+        {
+            href: `/${params.storeId}/colors`,
+            label: 'Colors',
+            active: pathname === `/${params.storeId}/colors`
+        },
+        {
             href: `/${params.storeId}/settings`,
             label: 'Settings',
             active: pathname === `/${params.storeId}/settings`
